8f4d46e0e58fdf64a36ac2c8cd36b0ac1219cc9e
Adriano Araujo Felisberto <adrianofelisberto1990@gmail.com>
2020-07-11 09:25:55 -0300

    feat: create the project

---
+++ src/main/java/br/com/symplus/challenger/bevolunteer/api/BevolunteerApplication.java
@@ -0,0 +1,13 @@
+package br.com.symplus.challenger.bevolunteer.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BevolunteerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BevolunteerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/symplus/challenger/bevolunteer/api/BevolunteerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.symplus.challenger.bevolunteer.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BevolunteerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
