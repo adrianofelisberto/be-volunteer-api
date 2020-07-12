08e213ceb20468a4d67c02c4894d312afc8d05f4
Adriano Araujo Felisberto <adrianofelisberto1990@gmail.com>
2020-07-12 05:59:25 -0300

    feat: add message config

--- src/main/java/br/com/symplus/challenger/bevolunteer/api/config/AppConfig.java
+++ src/main/java/br/com/symplus/challenger/bevolunteer/api/config/AppConfig.java
@@ -2,14 +2,25 @@ package br.com.symplus.challenger.bevolunteer.api.config;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ContainerConfig implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+public class AppConfig implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
 
 	@Override
 	public void customize(TomcatServletWebServerFactory factory) {
 		factory.setContextPath("/api/v1");
 	}
-	
+
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+		source.setBasenames("messages_pt-br");
+		source.setDefaultEncoding("UTF-8");
+		source.setUseCodeAsDefaultMessage(true);
+		return source;
+	}
+
 }
